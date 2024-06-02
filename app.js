12851996d75351d5fb3bafc81adcbe0af6b4dd9f
Ghazan <ghazankhan27@hotmail.com>
2024-06-02 17:37:24 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", (value) => {
+    io.emit("new-message", value);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
